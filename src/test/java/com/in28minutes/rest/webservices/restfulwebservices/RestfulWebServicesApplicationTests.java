e7e1cfb8c17997c6e2ce09979da108dcdcd6bdd6
badri <mohamedbadri136@gmail.com>
2022-07-18 00:13:20 +0200

    build project architecture

---
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,15 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertEquals(1, 1);
+    }
+
+}

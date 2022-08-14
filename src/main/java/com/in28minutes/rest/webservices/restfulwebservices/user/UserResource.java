0fdd3f7d00917f9a42d2cc94c9cf3557b7228345
badri <mohamedbadri136@gmail.com>
2022-08-14 21:51:26 +0200

    - add HATEOAS to link other resources - add internationalization bundles - add jackson dataformat for XML

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,6 +1,9 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -8,6 +11,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserResource {
@@ -17,18 +22,22 @@ public class UserResource {
 
     @GetMapping(path = "/users")
     public List<User> retrieveAllUsers() {
+
         return service.findAll();
     }
 
     @GetMapping(path = "/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
 
         User user = service.findOne(id);
         if (user == null)
             throw new UserNotFoundException("id-" + id);
-        return user;
 
-//        Resource<User>
+        EntityModel<User> entityModel = EntityModel.of(user);
+        Link link = WebMvcLinkBuilder.linkTo(
+                methodOn(this.getClass()).retrieveAllUsers()).withRel("all-users");
+        entityModel.add(link);
+        return entityModel;
     }
 
     @GetMapping(path = "/users/query")
